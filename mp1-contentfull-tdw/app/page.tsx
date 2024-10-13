e1b3423726f9d71d708df3eb067c6819ddc4d27b
underneth <davidmduarteoliveira@gmail.com>
2024-10-13 22:07:04 +0100

    Versão final

--- mp1-contentfull-tdw/app/page.tsx
+++ mp1-contentfull-tdw/app/page.tsx
@@ -13,17 +13,17 @@ function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Blog de TDW.
       </h1>
       <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
+        Aplicação de exemplo para o MP1 com:{" "}
         <a
           href="https://nextjs.org/"
           className="underline hover:text-success duration-200 transition-colors"
         >
           Next.js
         </a>{" "}
-        and{" "}
+        e{" "}
         <a
           href={CMS_URL}
           className="underline hover:text-success duration-200 transition-colors"
