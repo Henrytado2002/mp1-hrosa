9e2dbcb42748d855e7b3ceec391682ba3dca3ac3
Henrique Duarte Rosa <l51923@alunos.uevora.pt>
2024-10-22 21:51:36 +0100

    teste de lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,23 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
-
+ 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
-    rules:{
-      "no-unused-vars": "warn",
-      "no-undef": "warn"
-    }
-  }
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/prop-types": "off",
+      "@typescript-eslint/ban-ts-comment": [
+        "error",
+        {
+          "ts-ignore": false, // Permite o uso de @ts-ignore
+        },
+      ],
+    },
+  },
 ];
